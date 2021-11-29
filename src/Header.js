69c0ee7ca93e323a3c64f153ade8e0aa8286d449
Mustafa Siddiqui <mustafa.a.r.siddiqui@outlook.com>
2021-11-29 16:35:01 +0530

    mcanvis demo

---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {Navbar, Nav, Container, NavDropdown }from 'react-bootstrap'
+
+let Header = () => 
+<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Container>
+  <Navbar.Brand href="#home">MyCanadaVisas.com</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="me-auto">
+      <Nav.Link href="#Home">HOME</Nav.Link>
+      <Nav.Link href="#About">ABOUT</Nav.Link>
+      <NavDropdown title="SERVICES" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">STUDENT VISA</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">IMMIGRATION VISA</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">VISIT VISA</NavDropdown.Item>
+      </NavDropdown>
+      <Nav.Link href="#Contact">CONTACT US</Nav.Link>
+
+    </Nav>
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
+export default Header
